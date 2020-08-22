3117b92da27ad49610bfd30a0ba387d4b1a43da4
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-08-22 00:58:01 -0500

    added custom material theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import SubmitPost from './Components/SubmitPost/SubmitPost.js';
+import { CssBaseline } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <SubmitPost />
-    </div>
+      <React.Fragment>
+        <CssBaseline />
+        <SubmitPost />
+      </React.Fragment>
   );
 }
 
--- client/src/Components/SubmitPost/SubmitPost.js
+++ client/src/Components/SubmitPost/SubmitPost.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
+import { Grid } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -14,12 +14,6 @@ const useStyles = makeStyles((theme) => ({
             width: theme.spacing(100),
             height: theme.spacing(16),
         },
-    },
-    button: {
-        background: 'linear-gradient(45deg, #67EE94 10%, #67C1EE 99%)',
-        borderRadius: 3,
-        boxShadow: '0 3px 5px 2px #E0E0E0',
-        textAlign: 'center'
     }
 }));
 
@@ -42,11 +36,13 @@ export default function SubmitPost() {
                                         variant="outlined"
                                     />
                                 </div>
-                                <div className={classes.button}>
+                                <div>
                                     <Button
                                         label="submit"
                                         type="submit"
-                                        fullWidth={true}
+                                        fullWidth
+                                        color="primary"
+                                        variant="contained"
                                     >
                                         Send
                                     </Button>
