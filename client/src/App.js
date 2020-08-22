e4a157c3934e924b170b42db8a9e262beea53cc4
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-08-22 00:03:53 -0500

    added color to button

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,14 @@ import SubmitPost from './Components/SubmitPost/SubmitPost.js';
 function App() {
   return (
     <div className="App">
-      <h1>Vibes</h1>
-      
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <SubmitPost />
     </div>
   );
--- client/src/Components/SubmitPost/SubmitPost.js
+++ client/src/Components/SubmitPost/SubmitPost.js
@@ -2,36 +2,67 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 
-function SubmitPost() {
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(1),
+            width: theme.spacing(100),
+            height: theme.spacing(16),
+        },
+    },
+    button: {
+        background: 'linear-gradient(45deg, #67EE94 10%, #67C1EE 99%)',
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px #E0E0E0',
+        textAlign: 'center'
+    }
+}));
+
+export default function SubmitPost() {
+    const classes = useStyles();
+
     return (
-        <Grid container direction="column" justify="center" alignItems="center">
-            <h5>Submit Post Form in Progress...</h5>
-            <form>
-                <div>
-                    <TextField
-                        id="outlined-helperText"
-                        label="New Post"
-                        defaultValue="Say something..."
-                        helperText="..."
-                        variant="outlined"
-                        multiline
-                        rows={6}
-                    />
-                </div>
-                <div>
-                    <Button
-                        label="submit"
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                    >
-                        Send
-            </Button>
-                </div>
-            </form>
-        </Grid>
+        <div>
+            <Grid container spacing={1} direction="column" justify="center" alignItems="center">
+                <Grid item xs={12}>
+                    <div className={classes.paper}>
+                        <Paper elevation={2}>
+                            <form>
+                                <div>
+                                    <TextField
+                                        id="text"
+                                        label="Message"
+                                        defaultValue="Say something..."
+                                        helperText="..."
+                                        variant="outlined"
+                                    />
+                                </div>
+                                <div className={classes.button}>
+                                    <Button
+                                        label="submit"
+                                        type="submit"
+                                        fullWidth={true}
+                                    >
+                                        Send
+                                    </Button>
+                                </div>
+                            </form>
+                        </Paper>
+                    </div>
+                </Grid>
+            </Grid>
+
+        </div>
     );
 }
 
-export default SubmitPost;
+{/* <TextareaAutosize
+rowsMin={3}
+rowsMax={10}
+placeholder="Say something..."
+/> */}
