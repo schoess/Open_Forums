cf833a4afab71a53b73b5759f5139c9986a48f48
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-08-22 01:47:41 -0500

    centered content and made button smaller

--- client/src/Components/SubmitPost/SubmitPost.js
+++ client/src/Components/SubmitPost/SubmitPost.js
@@ -2,42 +2,40 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import { TextField } from '@material-ui/core';
 import { Button } from '@material-ui/core';
-import { Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > *': {
-            margin: theme.spacing(1),
-            width: theme.spacing(100),
-            height: theme.spacing(16),
-        },
+const myStyle = {
+    textField: {
+        width: "500px"
+    },
+    button: {
+        width: "100px"
     }
-}));
+}
 
-export default function SubmitPost() {
-    const classes = useStyles();
 
+
+function SubmitPost() {
     return (
         <div>
             <Grid container spacing={1} direction="column" justify="center" alignItems="center">
                 <Grid item xs={12}>
-                    <div className={classes.paper}>
-                        <Paper elevation={2}>
                             <form>
                                 <div>
                                     <TextField
+                                        style={myStyle.textField}
                                         id="text"
                                         label="Message"
                                         defaultValue="Say something..."
-                                        helperText="..."
                                         variant="outlined"
+                                        margin="normal"
+                                        multiline
+                                        rows={6}
+                                        fullWidth
                                     />
                                 </div>
                                 <div>
                                     <Button
+                                        style={myStyle.button}
                                         label="submit"
                                         type="submit"
                                         fullWidth
@@ -48,14 +46,14 @@ export default function SubmitPost() {
                                     </Button>
                                 </div>
                             </form>
-                        </Paper>
-                    </div>
                 </Grid>
             </Grid>
 
         </div>
     );
 }
+
+export default SubmitPost;
 
 {/* <TextareaAutosize
 rowsMin={3}
