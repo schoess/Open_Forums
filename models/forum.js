059461592cc19d02e655384a3dd73582addc6a2c
sathya <karpagasathya@gmail.com>
2020-08-22 12:25:15 -0500

    updated with required field

--- models/forum.js
+++ models/forum.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ForumSchema = new Schema({
-  forum_title: String,
-  forum_description: String,
-  category: String,
-  createdAt: Date,
-
+  forum_title: { type: String, required: true },
+  forum_description: { type: String, required: true },
+  category: { type: String, required: true },
+  date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Forum", ForumSchema);
