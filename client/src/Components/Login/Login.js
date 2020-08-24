f1c17decc9f2b9a2f4247f224144c24107523abd
sathya <karpagasathya@gmail.com>
2020-08-24 00:56:47 -0500

    login component

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-
+import { Button } from "@material-ui/core";
 
 const Login = () => {
-    const { loginWithRedirect } = useAuth0();
-    return (
-      <button
-        onClick={() => 
-          loginWithRedirect()
-        }
-      >
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  return (
+    !isAuthenticated && (
+      <Button color="secondary" onClick={() => loginWithRedirect()}>
         Log In
-      </button>
-    );
-}
+      </Button>
+    )
+  );
+};
 
 export default Login;
