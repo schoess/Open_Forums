bca33c144d01b6c33695d694511c93c035690ac1
sathya <karpagasathya@gmail.com>
2020-08-24 01:01:18 -0500

    created userinfo component just to check the login details and added that to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import Dashboard from './Components/Dashboard/Dashboard';
-import { CssBaseline } from '@material-ui/core';
-import NavBar from './Components/NavBar/NavBar.js';
-import Login from "./Components/Login/Login.js";
-
+import React from "react";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import { CssBaseline } from "@material-ui/core";
+import NavBar from "./Components/NavBar/NavBar.js";
+import UserInfo from "./Components/UserInfo/UserInfo";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <React.Fragment>
-      
-        <CssBaseline />
+      <CssBaseline />
       <NavBar />
-       <Login />
-        <Dashboard />
-      </React.Fragment>
+      <UserInfo />
+      <Dashboard />
+    </React.Fragment>
   );
 }
 
