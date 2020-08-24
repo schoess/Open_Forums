433171d9ef676806f2482519c58bbe394350839e
Anitha Venkatesan <anithamca68@gmail.com>
2020-08-24 15:58:38 -0500

    created react routes and made an axios call , added functionality in SubmitPost.js file for testing the data is posting in db

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Dashboard from './Components/Dashboard/Dashboard';
 import { CssBaseline } from '@material-ui/core';
 import NavBar from './Components/NavBar/NavBar.js';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
       <React.Fragment>
         <CssBaseline />
         <NavBar />
-        <Dashboard />
+        <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/forums" component={Dashboard} />
+        </Switch>
+
       </React.Fragment>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './theme'
+import theme from './theme';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
