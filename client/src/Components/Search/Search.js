8174185153196b30ea77a312ee8a5fbe28bfaa5a
sathya <karpagasathya@gmail.com>
2020-08-26 01:37:28 -0500

    search input fix

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -40,11 +40,11 @@ const useStyles = makeStyles((theme) => ({
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    paddingLeft: `calc(1em + ${theme.spacing(3)}px)`,
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
-      width: "20ch",
+      width: "50ch",
     },
   },
 }));
