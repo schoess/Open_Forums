14d034b57f9bedc6680d84031896a61d0e905b8e
sathya <karpagasathya@gmail.com>
2020-08-26 01:25:01 -0500

    alignment refactor

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,36 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import SubmitPost from '../SubmitPost/SubmitPost.js';
-import PostCard from '../PostCard/PostCard.js'
-import { Container } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import SubmitPost from "../SubmitPost/SubmitPost.js";
+import PostCard from "../PostCard/PostCard.js";
+import { Container } from "@material-ui/core";
 //import info from "../../assets/seed/data.json";
 import NavBar from "../NavBar/NavBar";
-import UserInfo from "../UserInfo/UserInfo";
 import forumApi from "../../utils/forum.api";
 
 const myStyle = {
-  textAlign: "center"
-}
+  textAlign: "center",
+};
 
 function Dashboard() {
-
   const [data, setData] = useState([]);
 
-  useEffect(
-    () => {
-      loadAllForum()
-    }, [])
+  useEffect(() => {
+    loadAllForum();
+  }, []);
   // Loads all forums and sets them to data
   function loadAllForum() {
-    forumApi.getAllForum()
-      .then(res =>
-        setData(res.data)
-      )
-      .catch(err => console.log(err));
-  };
+    forumApi
+      .getAllForum()
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }
   return (
     <div style={myStyle}>
       <NavBar />
-      <UserInfo />
       <Container>
         <SubmitPost />
         <PostCard data={data} />
