14cba29591cdc8daf8c893e99c328f4c84cc5cf6
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-08-25 20:50:56 -0500

    .

--- client/src/Components/PostCard/PostCard.js
+++ client/src/Components/PostCard/PostCard.js
@@ -14,33 +14,31 @@ const myStyle = {
 
 export default function PostCard(props) {
     return (
-<<<<<<< HEAD
-        <div className="post-card" style={myStyle.cardContainer}>
-        {
-            props.data.map(item => (
-                <Card style={myStyle.cardIndividual} key={item.id}>
-                <CardContent>
-                    <Typography>{item.User}</Typography>
-                    <Typography className="alignLeft" color="textSecondary" gutterBottom>
-                        {item.Title}
-                    </Typography>
-                    <Typography className="alignLeft" variant="body2" component="p">
-                        {item.PostBody}
-                    <br />
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button
-                    color="secondary"
-                    size="small"
-                    variant="contained"
-                    >
-                        Delete
-                    </Button>
-                </CardActions>
-            </Card>
-            ))
-=======
+        // <div className="post-card" style={myStyle.cardContainer}>
+        // {
+        //     props.data.map(item => (
+        //         <Card style={myStyle.cardIndividual} key={item.id}>
+        //         <CardContent>
+        //             <Typography>{item.User}</Typography>
+        //             <Typography className="alignLeft" color="textSecondary" gutterBottom>
+        //                 {item.Title}
+        //             </Typography>
+        //             <Typography className="alignLeft" variant="body2" component="p">
+        //                 {item.PostBody}
+        //             <br />
+        //             </Typography>
+        //         </CardContent>
+        //         <CardActions>
+        //             <Button
+        //             color="secondary"
+        //             size="small"
+        //             variant="contained"
+        //             >
+        //                 Delete
+        //             </Button>
+        //         </CardActions>
+        //     </Card>
+        //     ))
         <div className="post-card" style={myStyle}>
             {
                 props.data.map(item => (
@@ -59,7 +57,6 @@ export default function PostCard(props) {
                         </CardActions>
                     </Card>
                 ))
->>>>>>> 24822e35316b373925eb1c4d4c164ce394619264
 
             }
         </div>
