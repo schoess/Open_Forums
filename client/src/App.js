022be4a34c0a7cb2426bfa3a6c2d2f37abaf7026
Nick Schoess <schoessnick@gmail.com>
2020-08-26 21:02:42 -0500

    wrapped dashboard componment in context providerr

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Dashboard from "./Components/Dashboard/Dashboard";
 import { CssBaseline } from "@material-ui/core";
 import { Route, Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import {ForumProvider} from "./utils/ForumContext";
 
 function App() {
   const { isLoading } = useAuth0();
@@ -12,10 +13,12 @@ function App() {
   return (
     <React.Fragment>
       <CssBaseline />
+      <ForumProvider>
       <Switch>
         <Route exact path="/" component={Dashboard} />
         <Route exact path="/forums" component={Dashboard} />
       </Switch>
+      </ForumProvider>
     </React.Fragment>
   );
 }
