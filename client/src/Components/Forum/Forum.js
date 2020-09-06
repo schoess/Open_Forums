fd442e74b2cd82b7bd2260b0f1f51d058798fd06
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-09-05 19:29:17 -0500

    merge conflicts

--- client/src/Components/Forum/Forum.js
+++ client/src/Components/Forum/Forum.js
@@ -1,63 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useForumContext } from "../../contexts/ForumContext";
 import forumApi from "../../utils/forum.api";
-import { Card, CardContent, Typography,Avatar } from "@material-ui/core";
+import { Card, CardContent, Typography, Avatar } from "@material-ui/core";
 import ReplyCard from "../Reply/Replies";
 import NavBar from "../NavBar/NavBar";
 
 const myStyle = {
-<<<<<<< HEAD
-  cardContainer: {
-    textAlign: "center",
-    width: "700px",
-    margin: "0 auto",
-    paddingTop: "90px",
-  },
-  replyCardContainer: {
-    textAlign: "center",
-    width: "600px",
-    height: "200px",
-    margin: "0 auto",
-  },
-  cardIndividual: {
-    margin: "20px",
-  },
-  cardTitle: {
-    textAlign: "left",
-    fontWeight: "500px",
-  },
-  cardBody: {
-    textAlign: "left",
-  },
-};
-export default function (props) {
-  const { forum, setForum } = useForumContext();
-
-  useEffect(() => {
-    forumApi
-      .getById(props.match.params.forumId)
-      .then((res) => {
-        setForum(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-  return (
-    <div style={myStyle.cardContainer}>
-      <Card style={myStyle.cardIndividual}>
-        <CardContent>
-          <Typography style={myStyle.cardBody} variant="body2" component="p">
-            {forum.forum_title}
-          </Typography>
-          <Typography style={myStyle.cardBody} variant="body2" component="p">
-            {forum.forum_description}
-          </Typography>
-        </CardContent>
-      </Card>
-
-      <ReplyCard forumId={props.match.params.forumId} />
-    </div>
-  );
-=======
     cardContainer: {
         textAlign: "center",
         width: "700px",
@@ -107,5 +55,4 @@ export default function (props) {
         </div>
     );
 
->>>>>>> d305374d11251ad356dc2f524e96983d58456321
 }
