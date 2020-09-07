24a068cb3cfcee4b00cdf7d75d8db5d089f5120a
sathya <karpagasathya@gmail.com>
2020-09-06 22:00:11 -0500

    posting forums via modal

---
+++ client/src/Components/CreatePost/CreatePost.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import SubmitPost from "../SubmitPost/SubmitPost";
+import CreateIcon from "@material-ui/icons/Create";
+import { useSubmitPostModalContext } from "../../contexts/SubmitPostModalContext";
+
+const myStyle = {
+  CreatePostButton: {
+    backgroundColor: "primary",
+    paddingTop: "100px",
+    position: "sticky",
+    top: "0",
+  },
+};
+
+export default function CreatePost() {
+  const {
+    showSubmitPostModal,
+    setShowSubmitPostModal,
+  } = useSubmitPostModalContext();
+
+  const handleClickOpen = () => {
+    setShowSubmitPostModal(true);
+  };
+
+  const handleClose = () => {
+    setShowSubmitPostModal(false);
+  };
+
+  return (
+    <div style={myStyle.CreatePostButton}>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        <CreateIcon />
+        Create Post
+      </Button>
+      <Dialog
+        open={showSubmitPostModal}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Create Your own Post</DialogTitle>
+        <DialogContent>
+          <SubmitPost />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
---
+++ client/src/contexts/SubmitPostModalContext.js
@@ -0,0 +1,22 @@
+import React, { useState, createContext } from "react";
+
+export const SubmitPostModalContext = createContext();
+
+export const SubmitPostModalContextProvider = (props) => {
+  const [showSubmitPostModal, setShowSubmitPostModal] = useState(false);
+
+  return React.createElement(
+    SubmitPostModalContext.Provider,
+    {
+      value: {
+        showSubmitPostModal,
+        setShowSubmitPostModal,
+      },
+    },
+    props.children
+  );
+};
+
+export const useSubmitPostModalContext = () => {
+  return React.useContext(SubmitPostModalContext);
+};
