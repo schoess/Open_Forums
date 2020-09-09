50bd163271b9ab54fc4d6f617022bf990a90ea09
Nick Schoess <schoessnick@gmail.com>
2020-09-08 22:00:21 -0500

    added atlas

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable no-console */
 const express = require("express");
 const mongoose = require("mongoose");
@@ -18,17 +19,29 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // connect to mongodb
+// mongoose
+//   .connect(process.env.MONGODB_URI || "mongodb://localhost/openforums", {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//   })
+//   .then(() => console.log("Connected"))
+//   .catch((err) => console.log(err));
+
+// // below allows the findByIdAndDelete mongoose call to be used
+// mongoose.set("useFindAndModify", false);
+
+const connection = "mongodb+srv://forumGuy:x44ChTJmQysK3kwQ@cluster0.rmtku.mongodb.net/Open_Forums?retryWrites=true&w=majority";
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/openforums", {
-    useUnifiedTopology: true,
+  .connect(connection, {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(() => console.log("Connected"))
+  .then(() => console.log("Database Connected Successfully"))
   .catch((err) => console.log(err));
 
-// below allows the findByIdAndDelete mongoose call to be used
-mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
