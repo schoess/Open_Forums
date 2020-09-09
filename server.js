435d3de81b97e927a595829af84d524b4f07ca9b
Nick Schoess <schoessnick@gmail.com>
2020-09-08 22:03:45 -0500

    added process env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const routes = require("./routes");
 
