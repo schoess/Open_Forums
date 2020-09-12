e25e947f586f070b1b503d6978842dba640d23f6
sathya <karpagasathya@gmail.com>
2020-09-12 14:31:52 -0500

    Added card for top trending post

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,26 +1,41 @@
 import React from "react";
-import { Container, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import PostCard from "../PostCard/PostCard";
 import NavBar from "../NavBar/NavBar";
 import "./Dashboard.css";
 import CreatePost from "../CreatePost/CreatePost";
+import TopTrending from "../TopTrending/TopTrending";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 function Dashboard() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <NavBar isSearchEnable={true} />
-      <Container>
-        <Grid
-          container
-          spacing={1}
-          direction="row"
-          // "row-reverse"
-          justify="center"
-        >
-          <CreatePost />
-          <PostCard className="postCard" />
+
+      <Grid
+        container
+        spacing={1}
+        direction="row"
+        // "row-reverse"
+        // justify="center"
+      >
+        <Grid item xs={12} container>
+          <Grid item xs={8}>
+            <CreatePost />
+            <PostCard className="postCard" />
+          </Grid>
+          <Grid item xs={4}>
+            <TopTrending />
+          </Grid>
         </Grid>
-      </Container>
+      </Grid>
     </div>
   );
 }
